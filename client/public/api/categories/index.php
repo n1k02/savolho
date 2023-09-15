311205e3908b02597199e9de5759ed0c48fd1615
Amir <davronovichamirjon@mail.ru>
2023-09-15 08:19:24 +0500

    Amir

--- client/public/api/categories/index.php
+++ client/public/api/categories/index.php
@@ -1,7 +1,4 @@
-
 <?php
-
-
 include_once '../connector.php';
 
 $tbname = "categories";
@@ -10,41 +7,53 @@ $tbname = "categories";
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-// check method
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
- 
-    // Select request
-    $sql = "SELECT * FROM " . $tbname;
-    $result = $conn->query($sql);
+try {
+    // Попытка подключения к базе данных
+    $conn = new mysqli($servername, $username, $password, $dbname);
 
-    // Processing result and send to client
-    if ($result->num_rows > 0) {
-        $questions = array();
-        while ($row = $result->fetch_assoc()) {
-            $questions[] = $row;
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+
+    // check method
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        // Select request
+        $sql = "SELECT * FROM " . $tbname;
+        $result = $conn->query($sql);
+
+        // Processing result and send to client
+        if ($result->num_rows > 0) {
+            $questions = array();
+            while ($row = $result->fetch_assoc()) {
+                $questions[] = $row;
+            }
+            echo json_encode($questions);
+        } else {
+            echo json_encode(array("message" => "Question not found"));
         }
-        echo json_encode($questions);
-    } else {
-        echo json_encode(array("message" => "Question not found"));
-    }
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Insert request
+        $data = json_decode(file_get_contents("php://input"), true);
+        $id = 'DEFAULT';
+        $name = $data['name'];
 
-    // Insert request
-    $data = json_decode(file_get_contents("php://input"), true);
-    $id = 'DEFAULT';
-    $name = $data['name'];
+        $sql = "INSERT INTO $tbname (name) VALUES ('$name')";
 
-    $sql = "INSERT INTO $tbname (name) VALUES ('$name')";
-    
-    if ($conn->query($sql) === TRUE) {
-        echo json_encode(array("message" => "Question created successfully"));
+        if ($conn->query($sql) === TRUE) {
+            echo json_encode(array("message" => "Question created successfully"));
+        } else {
+            echo json_encode(array("error" => "Error when creating a question: " . $conn->error));
+        }
     } else {
-        echo json_encode(array("error" => "Error when creating a question: " . $conn->error));
+        http_response_code(405); // This method is not allowed 
+        echo json_encode(array("error" => "This method is not allowed"));
     }
-} else {
-    http_response_code(405); // This method is not allowed 
-    echo json_encode(array("error" => "This method is not allowed"));
+
+    // Закрываем соединение с базой данных
+    $conn->close();
+} catch (Exception $e) {
+    // Обработка ошибок подключения к базе данных
+    http_response_code(500); // Внутренняя ошибка сервера
+    echo json_encode(array("error" => $e->getMessage()));
 }
-
-$conn->close();
 ?>
